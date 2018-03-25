a6546fef98581a9032b591d657a7f444af91737f
AdamWD <falco1016@gmail.com>
2018-03-26 01:39:01 +0800

    make empty frame

--- fbMessenger/AppDelegate.swift
+++ fbMessenger/AppDelegate.swift
@@ -19,8 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
-        
-        
+        let layout = UICollectionViewFlowLayout()
+        let friendsController = FriendsController(collectionViewLayout: layout)
         window?.rootViewController = UINavigationController(rootViewController: friendsController)
         
         
