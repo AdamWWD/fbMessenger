1f60ab1b164572000a893160d875926b92963cc6
AdamWD <falco1016@gmail.com>
2018-03-27 10:58:30 +0800

    產生三個UICollectionViewCell，並加入UICollectionViewController 並註冊Cell

--- fbMessenger/ViewController.swift
+++ fbMessenger/ViewController.swift
@@ -9,18 +9,38 @@
 import UIKit
 
 class FriendsController: UICollectionViewController {
+    
+    private let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView?.backgroundColor = UIColor.red
-        
+        collectionView?.register(FriendCell.self, forCellWithReuseIdentifier: cellId)
         
     }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
     
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+}
 
+class FriendCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
     
-
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        backgroundColor = UIColor.blue
+    }
 }
 
